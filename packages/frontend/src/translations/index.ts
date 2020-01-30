a4c7a14562dd84bf548e2624d62dd65530e3c76e
Keimeno <constantin@metzworld.com>
2020-01-30 18:40:22 +0100

    updated core

--- packages/frontend/src/translations/index.ts
+++ packages/frontend/src/translations/index.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 
-import { Locale } from '@luminu/core';
+import { Locale } from '@luminu/core/frontend';
 const { getLocale } = Locale;
 
 import { en } from './en';
